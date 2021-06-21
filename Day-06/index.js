a8d9f596fdd5e9b312a94ffac3aa79611c4951cf
Vansh Shah <65898155+panthchristian@users.noreply.github.com>
2021-06-21 11:27:28 +0530

    improved color combination

--- Day-06/index.js
+++ Day-06/index.js
@@ -1,34 +1,43 @@
 //              Question 1
-
+console.log("Question 1");
 function isArray(e) {
   return Array.isArray(e);
 }
-// console.log(isArray("w3resource"));
-// console.log(isArray([1, 2, 4, 0]));
+console.log(isArray("w3resource"));
+console.log(isArray([1, 2, 4, 0]));
+console.log("");
 
 //              Question 2
+console.log("Question 2");
 function arrayClone(arr) {
   return arr.slice(0);
 }
-// console.log(arrayClone([1, 2, 4, 0]));
-// console.log(arrayClone([1, 2, [4, 0]]));
+console.log(arrayClone([1, 2, 4, 0]));
+console.log(arrayClone([1, 2, [4, 0]]));
+console.log("");
 
 //              Question 3
+console.log("Question 3");
 function first(arr, n = 1) {
   return n > 0 ? arr.slice(0, n) : [];
 }
-// console.log(first([7, 9, 0, -2]));
-// console.log(first([], 3));
-// console.log(first([7, 9, 0, -2], 3));
-// console.log(first([7, 9, 0, -2], 6));
-// console.log(first([7, 9, 0, -2], -3));
+console.log(first([7, 9, 0, -2]));
+console.log(first([], 3));
+console.log(first([7, 9, 0, -2], 3));
+console.log(first([7, 9, 0, -2], 6));
+console.log(first([7, 9, 0, -2], -3));
+console.log("");
 
 //               Question 4
-// myColor = ["Red", "Green", "White", "Black"];
-// console.log(myColor.join(","));
-// console.log(myColor.join("+"));
+console.log("Question 4");
+myColor = ["Red", "Green", "White", "Black"];
+console.log(myColor.join(","));
+console.log(myColor.join("+"));
+console.log("");
 
 //             Question 5
+
+console.log("Question 5");
 var arr1 = [3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3];
 var mf = 1; //default maximum frequency
 var m = 0; //counter
---
+++ README.md
@@ -0,0 +1,31 @@
+
+# Devsnest THA's
+
+This repo contains all the THA (Assignments) given in Devsnest's Front-end Module
+
+
+## Lessons Learned
+
+I came to know how HTML, CSS, Javascript work with each other to create the user experience. I faced some issues but my team supported me and helped me to overcome the issues.
+
+  
+## Demo
+
+|   Day   |  Live  |
+|---------|--------|
+|  Day 01 |        |
+|  Day 02 |        |
+|  Day 03 |        |
+|  Day 04 |        |
+|  Day 05 |        |
+|  Day 06 |        |
+|  Day 07 |        |
+|  Day 08 |        |
+|  Day 09 |        |
+
+  
+## Feedback
+
+If you have any feedback, please reach out to me at panthchristian4990@gmail.com
+
+  
