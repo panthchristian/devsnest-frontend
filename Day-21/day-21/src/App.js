0e1399f379f680c362e5943ddc0c38a9d0964542
Vansh Shah <65898155+panthchristian@users.noreply.github.com>
2021-08-04 21:41:49 +0530

    added day 21

---
+++ Day-21/day-21/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+// import Form from './Components/Form'
+import Data from "../src/components/Data";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Data />
+    </div>
+  );
+}
